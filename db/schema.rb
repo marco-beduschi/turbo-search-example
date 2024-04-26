d10a112742f18418c3ac1a1208a04ff4e8c2b985
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-04-26 18:23:53 -0300

    Chore: Add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_122930) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_212217) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 12, scale: 2
