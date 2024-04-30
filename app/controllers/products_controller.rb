e697a99637193513871a25dda3a0e85be0198e98
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-04-30 10:19:25 -0300

    Feat: Fetch content type products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.with_content_type(params[:content_type])
     @categories = Category.all
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :categories
+
+  scope :with_content_type, lambda { |content_type|
+    return if content_type.nil?
+
+    where(content_type:)
+  }
 end
