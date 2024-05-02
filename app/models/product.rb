99e9d33fbaffd11e2fd0f6018385b65ffe76fcb2
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-05-02 16:55:03 -0300

    Feat: Add scopes to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,20 @@ class Product < ApplicationRecord
 
     where(content_type:)
   }
+
+  scope :with_categories, lambda { |category_ids|
+                            return if category_ids.nil?
+
+                            joins(:categories).where(categories: { id: category_ids }).distinct
+                          }
+
+  scope :with_name, lambda { |name|
+    where('products.name LIKE ?', "%#{name}%")
+  }
+
+  scope :with_price_up_to, lambda { |price|
+                             return if price.nil?
+
+                             where('price <= ?', price)
+                           }
 end
