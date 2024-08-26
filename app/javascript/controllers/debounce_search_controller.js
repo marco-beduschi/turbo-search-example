b0a8f18964e5fe78116ca5f2a8420715837eb51a
Austin Kihara <austinkihara@Austins-MacBook-Pro-2.local>
2024-08-25 21:12:06 -0700

    implement debounce auto search in the products index

---
+++ app/javascript/controllers/debounce_search_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input"]
+
+  connect() {
+    this.timeout = null
+  }
+
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, 300) // Debounce for 300ms
+  }
+}
